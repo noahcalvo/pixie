ec0596655ad7098c54e341152db4988832cd5c5b
Noah Calvo <noah.calvo@gmail.com>
2023-05-04 14:55:43 -0500

    testing logging the build dir

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,7 @@
 const nextConfig = {
   // produces the build as HTML/CSS/JS assets so plain firebase plan can host. Remove for SSR
   output: "export",
+  // allows importing Markdown files using the raw-loader. This is to render README on the site
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
