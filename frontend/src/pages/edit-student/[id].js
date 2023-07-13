32e2ab8eb92c3aff37eb05a56234fff56c363831
Noah Calvo <noah.calvo@gmail.com>
2023-07-13 14:35:23 -0500

    Commit changes to edit-student page

--- frontend/src/pages/edit-student/[id].js
+++ frontend/src/pages/edit-student/[id].js
@@ -1,25 +1,27 @@
 import React from "react";
 import EditStudent from "../../components/edit-student.component";
 
-const EditStudentPage = ({ id }) => {
-  return <EditStudent id={id} />;
+const EditStudentPage = () => {
+  return <EditStudent />;
 };
 
-export async function getStaticProps(context) {
-  const { id } = context.params;
+// UNABLE TO USE THE FOLLOWING CODE WITH FIREBASE FREE TIER
 
-  return {
-    props: {
-      id,
-    },
-  };
-}
+// export async function getStaticProps(context) {
+//   const { id } = context.params;
 
-export async function getStaticPaths() {
-  // This function can be used to specify the dynamic routes that should be generated at build time.
-  // For example, you can fetch a list of IDs from an external API and generate a route for each ID.
-  // In this case, since we're using a dynamic route with a single ID parameter, we'll just return an empty array.
-  return { paths: [], fallback: true };
-}
+//   return {
+//     props: {
+//       id,
+//     },
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   // This function can be used to specify the dynamic routes that should be generated at build time.
+//   // For example, you can fetch a list of IDs from an external API and generate a route for each ID.
+//   // In this case, since we're using a dynamic route with a single ID parameter, we'll just return an empty array.
+//   return { paths: [], fallback: true };
+// }
 
 export default EditStudentPage;
